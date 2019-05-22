1462e7974d1edc8904fa29599d3359770da5ce4f
JinLong <kenny19940602@163.com>
2019-05-22 11:12:59 +0800

    springcloud  eureka-server   eureka-client

---
+++ provide-user/src/main/java/cn/tedu/provideuser/ProvideUserApplication.java
@@ -0,0 +1,15 @@
+package cn.tedu.provideuser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class ProvideUserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProvideUserApplication.class, args);
+    }
+
+}
---
+++ provide-user/src/main/java/cn/tedu/provideuser/controller/HelloController.java
@@ -0,0 +1,15 @@
+package cn.tedu.provideuser.controller;
+
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @RequestMapping("hello/{name}")
+    public String hello(@PathVariable  String name){
+        return "spring boot "+name;
+    }
+}
